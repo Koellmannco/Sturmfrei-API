142a2443e5a6be40e7a0495db603e9e3252d7ac3
arbrog <arbrog@gmail.com>
2017-07-13 18:29:34 -0700

    fixed config

--- project/app.py
+++ project/app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL")
 db = SQLAlchemy(app)
 
 @app.route('/')
--- project/config.py
+++
@@ -1,3 +0,0 @@
-import os
-
-SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI")
