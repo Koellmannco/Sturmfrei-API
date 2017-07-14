4902c94d5b7ed2bc645b4d8e596fcbd9b5e7ac65
arbrog <arbrog@gmail.com>
2017-07-13 18:45:03 -0700

    database

--- project/app.py
+++ project/app.py
@@ -6,10 +6,22 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL")
 db = SQLAlchemy(app)
-
+db.create_all()
 @app.route('/')
 def index():
     return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.Unicode)
+    username = db.Column('username', db.Unicode)
+    email = db.Column('email', db.Unicode)
+    password = db.Column('password', db.Unicode)
+    register_date = db.Column('register_date',db.TIMESTAMP, default=datetime.now())
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
