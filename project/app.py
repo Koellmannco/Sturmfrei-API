0209890ba07b41ff1996c03da50ccb7e041a594e
arbrog <arbrog@gmail.com>
2017-07-23 12:37:35 -0700

    moved stuff

--- project/app.py
+++ project/app.py
@@ -6,7 +6,6 @@ import os
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL")
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://sturmfrei_api_DB:2Fm82iFHgmFCR8DBJsFe@sturmfrei-api-database01.cnxqjx6tjodx.us-east-1.rds.amazonaws.com/sturmfrei_api"
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -18,12 +17,6 @@ class User(db.Model):
     password = db.Column("password", db.Unicode(32), nullable=False)
     register_date = db.Column("register_date", db.TIMESTAMP, default=datetime.now())
 
-#db.drop_all()
-#db.create_all()
-User1 = User(name='Austin4',username='arbrog4',email='arbrog4@gmail.com', password='password123')
-db.session.add(User1)
-db.session.commit()
-
 @app.route('/')
 def index():
     return "Hello, World! This is the Sturmfrei API"
@@ -33,3 +26,9 @@ if __name__ == '__main__':
 
     def __repr__(self):
         return '<User %r>' % (self.name)
+
+db.drop_all()
+db.create_all()
+User1 = User(name='Austin4',username='arbrog4',email='arbrog4@gmail.com', password='password123')
+db.session.add(User1)
+db.session.commit()
