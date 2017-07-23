aa353338987e9a22f458955484e1c8d076489c81
arbrog <arbrog@gmail.com>
2017-07-23 11:59:05 -0700

    added testing for db

--- project/app.py
+++ project/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-import os
+#import os
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL")
@@ -11,12 +11,18 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column('id', db.Integer, primary_key=True)
-    name = db.Column('name', db.Unicode)
-    username = db.Column('username', db.Unicode)
-    email = db.Column('email', db.Unicode)
-    password = db.Column('password', db.Unicode)
-    register_date = db.Column('register_date',db.TIMESTAMP, default=datetime.now())
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.Unicode(30))
+    username = db.Column(db.Unicode(25), nullable=False)
+    email = db.Column(db.Unicode(40), nullable=False)
+    password = db.Column(db.Unicode(32), nullable=False)
+    register_date = db.Column(db.TIMESTAMP, default=datetime.now())
+
+db.drop_all()
+db.create_all()
+User1 = User(name='Austin',username='arbrog',email='arbrog@gmail.com', password='password123')
+db.session.add(User1)
+db.session.commit()
 
 @app.route('/')
 def index():
