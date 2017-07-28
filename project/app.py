8fbe6d37f333a6e3198d06085ed0a001de11fd03
arbrog <arbrog@gmail.com>
2017-07-28 15:23:23 -0400

    using flask=restful

--- project/app.py
+++ project/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 from project.database import db
 from project.user import User
-import warnings
+from flask_restful import Resource, Api
 
 import os
 
 app = Flask(__name__)
+api = Api(app)
 # app.secret_key = 'Test123'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 db.init_app(app)
@@ -15,27 +16,28 @@ db.init_app(app)
 def index():
     return "Hello, World! This is the Sturmfrei API"
 
+class getUsers(Resource):
+    def get(self):
+        userList = []
+        for user in db.session.query(User.username).order_by(User.id):
+            userList.append(user)
+        return jsonify({'userList': userList})
 
-@app.route('/getUsers', methods=['GET'])
-def getUsers():
-    userList = []
-    for user in db.session.query(User.username).order_by(User.id):
-        userList.append(user)
-    return jsonify({'userList': userList})
+api.add_resource(getUsers, '/getUsers')
 
+class getUser(Resource):
+    def get(self, user_id):
+        user = db.session.query(User).first()
+        return jsonify({'user': user})
 
-@app.route('/getUsers/<int:user_id>', methods=['GET'])
-def getUser(user_id):
-    user = db.session.query(User).first()
-    return jsonify({'user': user})
-
+api.add_resource(getUser, '/getUsers/<int:user_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-with app.app_context():
-    db.drop_all()
-    db.create_all()
-    User1 = User(name='Austin1',username='arbrog1',email='arbrog1@gmail.com', password='password123')
-    db.session.add(User1)
-    db.session.commit()
+#with app.app_context():
+   # db.drop_all()
+  #  db.create_all()
+  #  User1 = User(name='Austin1',username='arbrog1',email='arbrog1@gmail.com', password='password123')
+   # db.session.add(User1)
+   # db.session.commit()
