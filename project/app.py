35ec2c74fa0c9dacbc07e6873b180b8c2c27ffc7
arbrog <axb6999@rit.edu>
2017-07-28 18:53:42 -0400

    not debug

--- project/app.py
+++ project/app.py
@@ -36,11 +36,11 @@ api.add_resource(getUser, '/getUsers/<int:user_id>')
 if __name__ == '__main__':
     app.run(debug=True)
 
-with app.app_context():
-    db.drop_all()
-    db.create_all()
-    User1 = User(firstname='Austin', lastname='Brogan', username='arbrog',email='arbrog14@gmail.com', password='password123')
-    db.session.add(User1)
-    db.session.commit()
-    mixer.init_app(app)
-    users = mixer.cycle(10).blend(User)
+# with app.app_context():
+#     db.drop_all()
+#     db.create_all()
+#     User1 = User(firstname='Austin', lastname='Brogan', username='arbrog',email='arbrog14@gmail.com', password='password123')
+#     db.session.add(User1)
+#     db.session.commit()
+#     mixer.init_app(app)
+#     users = mixer.cycle(10).blend(User)
