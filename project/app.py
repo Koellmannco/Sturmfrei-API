2319d4e97716a365b3bd74f909b317162e55b299
arbrog <axb6999@rit.edu>
2017-07-30 19:31:34 -0400

    more testing

--- project/app.py
+++ project/app.py
@@ -42,7 +42,8 @@ class Users(Resource):
         user = schema.load(request.data)
         #db.session.add(user)
         #db.session.commit()
-        return jsonify({'result': request.data})
+        return print(request.data)
+        #return jsonify({'result': user})
 
 api.add_resource(Users, '/Users/')
 
