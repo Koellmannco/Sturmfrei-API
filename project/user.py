d737993fefbab6d5cc1b7809e43ac567f94d94b5
arbrog <arbrog@gmail.com>
2017-08-04 12:35:56 -0400

    fixed auth (hopefully)

--- project/user.py
+++ project/user.py
@@ -60,6 +60,7 @@ class User(db.Model):
 @auth.verify_password
 def verify_password(username_or_token, password):
     # first try to authenticate by token
+    print(username_or_token & ' : ' & password)
     user = User.verify_auth_token(username_or_token)
     if not user:
         user = User.get(username_or_token)  # based on credentials
