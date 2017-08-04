2d05c51e583cd9e7cc61c84291cb2fcf316cff4c
arbrog <axb6999@rit.edu>
2017-08-04 17:45:38 -0400

    auth testing

--- project/user.py
+++ project/user.py
@@ -68,6 +68,7 @@ class User(db.Model):
 @auth.verify_password
 def verify_password(username_or_token, password):
     # first try to authenticate by token
+    print(username_or_token)
     user = User.verify_auth_token(username_or_token)
     if not user:
         user = User.get(username=username_or_token)  # based on credentials
